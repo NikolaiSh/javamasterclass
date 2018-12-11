50f1cc8677846d978a19ca2425c227f4ef40d3a3
Nikolaj Szwarsalon <shvarsalon@gmail.com>
2018-12-12 00:39:10 +0100

    Lecture #95 - LinkedList challenge

---
+++ LinkedListChallenge/src/com/company/Album.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Album {
+    private String title;
+    private ArrayList<Song> songs;
+
+    public Album(String title) {
+        this.title = title;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String songTitle, double songDuration) {
+        if(findSong(songTitle) != null) {
+            System.out.println("Such song is already on the album");
+            return false;
+        }
+
+        this.songs.add(new Song(songTitle,songDuration));
+        System.out.println("Song " + songTitle + " was added to album " + this.getTitle());
+        return true;
+    }
+
+    public Song findSong(String songTitle) {
+        for(int i = 0; i < this.songs.size(); i++) {
+            if(this.songs.get(i).getTitle().equals(songTitle)){
+                return this.songs.get(i);
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+}
